c389c917ee166269d59208d2071430d316d37081
User <User@DESKTOP-ONQUMV3.lan>
2025-03-19 16:00:14 +0200

    PART_1A

---
+++ src/part_1/Part_1.java
@@ -0,0 +1,30 @@
+
+package part_1;
+
+/**
+ *Student name: Ndaedzo tshiovhe
+ * Student number: ST 10487456
+ * Assignment part 1
+ * 
+ * @author User
+ */
+public class Part_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Registration registration = new Registration();
+
+    }
+
+    static class NewJFrame {
+
+        public NewJFrame() {
+        }
+    }
+    }
+        
+        
+        
+   
---
+++ src/part_1/login.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package part_1;
+
+/**
+ *
+ * @author User
+ */
+public class login {
+
+    login(Part_1.NewJFrame newjframe) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
